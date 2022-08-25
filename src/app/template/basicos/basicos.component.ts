c5fdcc416ae1fa9a4a05304115f35fd1e64b8f12
Oscar Davila <oscar7822002@gmail.com>
2022-08-25 08:52:18 -0500

    Template: Validar número igual mayor a 0

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -15,8 +15,13 @@ export class BasicosComponent implements OnInit {
   }
 
   nombreValido(): boolean {
-    return this.miFormulario?.controls?.['producto'].invalid &&
-           this.miFormulario?.controls?.['producto'].touched;
+    return this.miFormulario?.controls['producto']?.invalid &&
+           this.miFormulario?.controls['producto']?.touched;
+  }
+
+  precioValido(): boolean {
+    return this.miFormulario?.controls['precio']?.touched &&
+           this.miFormulario?.controls['precio']?.value < 0;
   }
 
   guardar() {
