75e2933959e8ce2f1821b11b33a82f53cfcc7cd2
Oscar Davila <oscar7822002@gmail.com>
2022-08-26 09:20:58 -0500

    Validaciones básicas - Forms Validator

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -14,9 +14,9 @@ export class BasicosComponent {
   // })
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: [''],
-    precio: [0],
-    existencias: [0],
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    precio: [0, [Validators.required, Validators.min(0)]],
+    existencias: [0, [Validators.required, Validators.min(0)]],
   })
 
   constructor(private fb: FormBuilder) { }
