d26e92cbef81598b43939bc2b78a812c09b0a68a
Oscar Davila <oscar7822002@gmail.com>
2022-08-26 08:59:11 -0500

    Primeros pasos en formularios reactivos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html'
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
+
+  miFormulario: FormGroup = new FormGroup({
+    'nombre': new FormControl('')
+  })
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
