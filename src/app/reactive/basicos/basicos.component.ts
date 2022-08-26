23fdf57ff249c2a89fc76b086470e2c1d66efb06
Oscar Davila <oscar7822002@gmail.com>
2022-08-26 09:38:55 -0500

    Submit del formulario

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html'
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit {
 
   // miFormulario: FormGroup = new FormGroup({
   //   nombre: new FormControl(''),
@@ -21,9 +21,31 @@ export class BasicosComponent {
 
   constructor(private fb: FormBuilder) { }
 
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: '',
+      precio: 0,
+      existencias: 1
+    });
+  }
+
   campoEsValido(campo: string) {
     return this.miFormulario.controls[campo].errors &&
            this.miFormulario.controls[campo].touched
+  }
+
+  guardar() {
+
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset({
+      nombre: '',
+      precio: 0,
+      existencias: 1
+    });
   }
 
 
