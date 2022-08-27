1beeb385aebf928b27d0c8c93a27a2dd912acf74
Oscar Davila <oscar7822002@gmail.com>
2022-08-27 15:49:41 -0500

    Evaluar un email

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -9,14 +9,20 @@ export class RegistroComponent implements OnInit {
 
   // TODO: Temporal
   nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['', [ Validators.required, Validators.pattern(this.nombreApellidoPattern) ]]
+    nombre: ['', [ Validators.required, Validators.pattern(this.nombreApellidoPattern) ]],
+    email: ['', [ Validators.required, Validators.pattern(this.emailPattern) ]],
   })
 
   constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Oscar Davila',
+      email: 'test1@test.com'
+    })
   }
 
   campoNoValido(campo: string) {
